4ecb7557f0301efa88b2476e2add08a6efdd3ac8
harikrishnan-hk <krishnanhari@outlook.com>
2024-07-20 05:33:38 -0400

    added-backend-statelock-s3

---
+++ environments/staging/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "staging-tfstate-bucket"
+    key            = "staging/terraform.tfstate"
+    region         = "ca-central-1"
+    dynamodb_table = "staging-tfstate-lock"
+    encrypt        = true
+  }
+}
